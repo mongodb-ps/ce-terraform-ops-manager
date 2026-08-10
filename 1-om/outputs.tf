c96487fb6b05002f4a9e6d3016c25378f891fa0a
Yaoxing Zhang <yaoxing.zhang@gmail.com>
2026-08-10 16:36:30 +0200

    add first user to output.

--- 1-om/outputs.tf
+++ 1-om/outputs.tf
@@ -3,6 +3,15 @@ output "backing_db_credentials" {
   sensitive = true
 }
 
+output "first_user" {
+  description = "Credentials of the first Ops Manager user"
+  value = {
+    email = local.first_user.email
+    pwd   = local.first_user.pwd
+  }
+  sensitive = true
+}
+
 output "om_access_url" {
   description = "Access URL for Ops Manager"
   value       = "http://${module.om_app.instance_public_dns[0]}:8080/"
